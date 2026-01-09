8ed3dc91c62ef43711f20e0f9108c51f77145a1e
riricardo <ricardo.augusto.cardona@gmail.com>
2026-01-09 00:53:37 +0000

    Finishing up

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ function setup(id, fn) {
 
 setup("btn1", () => {
   console.log(
-    "This is a log closure example using the command pattern. The closure has a message array that stay in memory. It can add messages and print all.\n"
+    "This is a log closure example using the command pattern. The closure has a message array that stay in memory. It can add messages and print all."
   );
 
-  function logClosure() {
-    let messages = [];
-
+  function createClosureLogger(messages) {
     return {
       log: (msg) => {
         messages.push(msg);
@@ -21,7 +19,8 @@ setup("btn1", () => {
     };
   }
 
-  const logger = logClosure();
+  let messages = [];
+  const logger = createClosureLogger(messages);
   logger.log("First message");
   logger.log("Second message");
   logger.print();
@@ -30,9 +29,47 @@ setup("btn1", () => {
 });
 
 setup("btn2", () => {
-  alert("Button 2 clicked!");
+  console.log(
+    "The closure here is used to create a factory pattern having a custom way to define the logger."
+  );
+
+  function createClosureLogger(prefix) {
+    return function (msg) {
+      console.log(`[${prefix}] ${msg}`);
+    };
+  }
+
+  const infoLogger = createClosureLogger("INFO");
+  const errorLogger = createClosureLogger("ERROR");
+
+  infoLogger("This is an informational message.");
+  errorLogger("This is an error message.");
 });
 
 setup("btn3", () => {
-  alert("Button 3 clicked!");
+  console.log(
+    "This closure example demonstrates the usage of module pattern considering a situation of theme control."
+  );
+
+  function createClosureThemeManager() {
+    let theme;
+    let language;
+
+    return {
+      setTheme: (newTheme) => {
+        theme = newTheme;
+      },
+      setLanguage: (newLanguage) => {
+        language = newLanguage;
+      },
+      getSettings: () => {
+        return { theme, language };
+      },
+    };
+  }
+
+  const themeManager = createClosureThemeManager();
+  themeManager.setTheme("dark");
+  themeManager.setLanguage("en");
+  console.log(themeManager.getSettings());
 });
